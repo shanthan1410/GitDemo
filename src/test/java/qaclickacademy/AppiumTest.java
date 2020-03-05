883fa6e2f1e5e68fbf6faa63def4fd1ef251792e
peesa <shanthan1410@gmail.com>
2020-03-05 14:41:34 -0500

    first commit in develop branch

--- src/test/java/qaclickacademy/AppiumTest.java
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -13,5 +13,15 @@ public class AppiumTest {
 	public void NativeAppIos(){
 		System.out.println("NativeAppIos");
 	}
+	
+	@Test
+	public void realAppAndroid(){
+		System.out.println("realAppAndroid");
+	}
+	
+	@Test
+	public void realAppIos(){
+		System.out.println("realAppIos");
+	}
 
 }
